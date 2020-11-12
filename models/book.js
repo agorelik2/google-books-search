cca9dbb1308d97a6a1a2cd1c0922a6961263d137
Red Fairy <agorelik2@gmail.com>
2020-11-12 14:53:59 -0500

    initial setup, routes, app.js, server.js

---
+++ models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+  googleId: { type: String, required: true, unique: false },
+  image: { type: String, required: true },
+  title: { type: String, required: true },
+  authors: [{ type: String, required: true }],
+  description: { type: String, required: true },
+  link: { type: String, required: true },
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+const app = express();
+const PORT = process.env.PORT || 3001;
+require("dotenv").config();
+
+//react access the api key in the .env
+//process.env.REACT_APP_API_KEY;
+//process.env.dbuser;
+//process.env.dbpassword;
+
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve up static assets (usually on heroku)
+//comment this out to launch
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//this will be part of the code that we leave in to deploy on heroku
+//app.use(express.static("client/build"));
+
+// Add routes, both API and view
+app.use(routes);
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gbooks");
+
+// mongoose.connect(process.env.MONGODB_URI || `mongodb://${dbuser}:${dbpassword}@ds..../heroku_....`);
+
+// Start the API server
+app.listen(PORT, function () {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
