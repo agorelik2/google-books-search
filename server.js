d9473875d785f4774ccb26dca56e5871c433f908
Red Fairy <agorelik2@gmail.com>
2020-11-13 14:40:57 -0500

    change to server.js in attempt to deploy

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
-require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// const mongooseConfig = {
+//   useUnifiedTopology: true,
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+// };
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/gbooks";
 //react access the api key in the .env
 //process.env.REACT_APP_API_KEY;
-
+require("dotenv").config();
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-// Serve up static assets (usually on heroku)
-//comment this out to launch
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-//app.use(express.static("public"));
-
-//this will be part of the code that we leave in to deploy on heroku
-//app.use(express.static("client/build"));
-
-// Add routes, both API and view
-app.use(routes);
 
 // Connect to the Mongo DB
 //mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gbooks");
@@ -34,7 +24,15 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/gbooks", {
   useCreateIndex: true,
   useFindAndModify: false,
 });
-// mongoose.connect(process.env.MONGODB_URI || `mongodb://${dbuser}:${dbpassword}@ds..../heroku_....`);
+
+// Serve up static assets (usually on heroku)
+//app.use(express.static("public"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
 
 // Start the API server
 app.listen(PORT, function () {
